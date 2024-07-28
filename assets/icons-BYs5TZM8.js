287e9fdc8d2b4ddb970ac9dd1faa4218e14accdd
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-28 21:12:54 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs98_front_Vanlsh@1fd2bafa39ec032fd2996729f2b8e3b5ab80b62d 🚀

--- assets/icons-BYs5TZM8.js
+++
@@ -1,2 +0,0 @@
-import{j as s,L as o}from"./index-DqmA4O_d.js";const t="_logo_1v2e6_1",r={logo:t},c=()=>s.jsx(o,{to:"/",className:r.logo,children:"AquaTrack"}),n="/assets/icons-BrKUAJ2l.svg";export{c as L,n as s};
-//# sourceMappingURL=icons-BYs5TZM8.js.map
---
+++ assets/icons-vKTPq412.js
@@ -0,0 +1,2 @@
+import{j as s,L as o}from"./index-D8yeLwKQ.js";const t="_logo_1v2e6_1",n={logo:t},a=()=>s.jsx(o,{to:"/",className:n.logo,children:"AquaTrack"}),c="/node_fs98_front_Vanlsh/assets/icons-BrKUAJ2l.svg";export{a as L,c as s};
+//# sourceMappingURL=icons-vKTPq412.js.map
