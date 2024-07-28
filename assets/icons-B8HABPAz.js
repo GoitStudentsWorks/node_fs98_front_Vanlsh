79b4592d4828385d7b5b81072b8e9e02459cd825
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-28 21:06:37 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs98_front_Vanlsh@a9d361d8742e56d8c9c3cbdd94aa5aad033f4d8c 🚀

--- assets/icons-B8HABPAz.js
+++
@@ -1,2 +0,0 @@
-import{j as s,L as o}from"./index-Xn8S7hQC.js";const t="_logo_1v2e6_1",r={logo:t},c=()=>s.jsx(o,{to:"/",className:r.logo,children:"AquaTrack"}),n="/assets/icons-BrKUAJ2l.svg";export{c as L,n as s};
-//# sourceMappingURL=icons-B8HABPAz.js.map
